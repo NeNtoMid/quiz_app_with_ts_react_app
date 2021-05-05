e85d47d922ad43166ad53898833e99fff69cd66c
Dominik <NeNtoMid0@gmail.com>
2021-05-05 17:29:13 +0200

    Fix some typos and changed questions reducer in terms of TS logic

--- src/store/reducers/questions.ts
+++ src/store/reducers/questions.ts
@@ -7,17 +7,68 @@ import {
 	QuestionsStateInterface,
 } from './../../shared/interfaces/questions.interface';
 
-interface Payload {
-	data?: AxiosResponseInterface;
-	message?: string;
-	id?: number;
-	userAnswerIsProper?: boolean;
-}
+type Actions =
+	| {
+			type: ActionsType.FETCH_QUESTIONS_SUCCESS;
+			payload: { data: AxiosResponseInterface };
+	  }
+	| {
+			type: ActionsType.CHANGE_QUESTION_NUMBER;
+			payload: { id: number; userAnswerIsProper: boolean };
+	  }
+	| {
+			type: ActionsType.CHANGE_QUESTION_VIEW;
+			payload: { userAnswerIsProper: boolean };
+	  };
 
-export interface Action {
-	type: ActionsType;
-	payload?: Payload;
-}
+const fetchQuestionsSuccess = (
+	draft: QuestionsStateInterface,
+	actionPayload: { data: AxiosResponseInterface }
+) => {
+	draft.data = actionPayload.data.results.map((el) => ({
+		...el,
+		answers: [...el.incorrect_answers, el.correct_answer].sort(
+			() => Math.random() - 0.5
+		),
+	}));
+
+	draft.totalQuestions = actionPayload.data.results.length;
+	draft.error = actionPayload.data.response_code !== 0;
+
+	draft.score = 0;
+	draft.questionNum = 0;
+	draft.isStarted = true;
+	draft.clicked = false;
+};
+
+const changeQuestionNumber = (
+	draft: QuestionsStateInterface,
+	actionPayload: { id: number; userAnswerIsProper: boolean }
+) => {
+	draft.data = draft.data?.map((el, index) => {
+		if (index === draft.questionNum) {
+			el.user_answer = actionPayload.userAnswerIsProper;
+			el.chosen = actionPayload.id.toString();
+		}
+		return { ...el };
+	});
+
+	draft.clicked = true;
+	draft.isStarted = draft.questionNum === 9 ? false : true;
+};
+
+const changeQuestionView = (
+	draft: QuestionsStateInterface,
+	actionPayload: { userAnswerIsProper: boolean }
+) => {
+	draft.questionNum =
+		draft.questionNum < 9 ? draft.questionNum + 1 : draft.questionNum;
+
+	draft.score = actionPayload.userAnswerIsProper
+		? draft.score + 1
+		: draft.score;
+	draft.clicked = false;
+};
 
 const initialState = {
 	data: undefined,
@@ -29,63 +80,7 @@ const initialState = {
 	totalQuestions: 0,
 };
 
-const fetchQuestionsSuccess = (
-	draft: QuestionsStateInterface,
-	actionPayload: Payload | undefined
-) => {
-	if (actionPayload && actionPayload.data) {
-		draft.data = actionPayload.data.results.map((el) => ({
-			...el,
-			answers: [...el.incorrect_answers, el.correct_answer].sort(
-				() => Math.random() - 0.5
-			),
-		}));
-
-		draft.totalQuestions = actionPayload.data.results.length;
-		draft.error = actionPayload.data.response_code !== 0;
-	}
-
-	draft.score = 0;
-	draft.questionNum = 0;
-	draft.isStarted = true;
-	draft.clicked = false;
-};
-
-const changeQuestionNumber = (
-	draft: QuestionsStateInterface,
-	actionPayload: Payload | undefined
-) => {
-	if (actionPayload && actionPayload.userAnswerIsProper !== undefined) {
-		draft.data = draft.data?.map((el, index) => {
-			if (index === draft.questionNum) {
-				el.user_answer = actionPayload.userAnswerIsProper;
-				el.chosen = actionPayload.id?.toString();
-			}
-			return { ...el };
-		});
-
-		draft.clicked = true;
-		draft.isStarted = draft.questionNum === 9 ? false : true;
-	}
-};
-
-const changeQuestionView = (
-	draft: QuestionsStateInterface,
-	actionPayload: Payload | undefined
-) => {
-	draft.questionNum =
-		draft.questionNum < 9 ? draft.questionNum + 1 : draft.questionNum;
-
-	draft.score = actionPayload?.userAnswerIsProper
-		? draft.score + 1
-		: draft.score;
-	draft.clicked = false;
-};
-
-const questionsReducer = (
-	state: QuestionsStateInterface = initialState,
-	action: Action
-) => {
+const questionsReducer = (state = initialState, action: Actions) => {
 	return produce(state, (draft) => {
 		switch (action.type) {
 			case ActionsType.FETCH_QUESTIONS_SUCCESS:
@@ -99,9 +94,6 @@ const questionsReducer = (
 
 			case ActionsType.CHANGE_QUESTION_VIEW:
 				changeQuestionView(draft, action.payload);
-				break;
-
-			default:
 				break;
 		}
 	});
